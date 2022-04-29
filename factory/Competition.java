f3424288a29e736bf01de092a951d9741fbaeda9
danzillo <dan4ikhom@gmail.com>
2022-04-29 20:21:48 +0700

    facrtory

---
+++ factory/Competition.java
@@ -0,0 +1,15 @@
+package factory;
+
+public class Competition {
+    public void addDiscription(){
+        //для добасвления описания
+    }
+
+    public void addSportsmanList(){
+        //для добавления списка участников
+    }
+
+    public void addMap(){
+        //для добавления карты соревнований
+    }
+}
---
+++ factory/EventFactory.java
@@ -0,0 +1,5 @@
+//package factory;
+//
+//public class EventFactory {
+//    public Events createEvent
+//}
